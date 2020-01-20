c781ac61336f1fc639400e04a8bbc640d2a36ba6
Janessa Tran <tranjanessa@gmail.com>
2020-01-20 13:56:06 -0600

    Add a wee bit of styling to make sure that it works

--- Gemfile
+++ Gemfile
@@ -35,8 +35,8 @@ gem 'jbuilder', '~> 2.5'
 
 gem "figaro"
 gem "google_drive"
-gem "haml-rails"
 gem "haml"
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- app/controllers/wins_controller.rb
+++ app/controllers/wins_controller.rb
@@ -1,4 +1,4 @@
-class WinsController < ActionController::Base
+class WinsController < ApplicationController 
   def index
     @small_wins = GoogleSheetsReader.new.get_wins_as_csv
     @csv_table = CSV.parse(@small_wins, headers: true)
